1ffd0d5561163e6f2254612935979c169896487a
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-05-21 00:38:07 +0700

    feat: detail project

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,9 +1,13 @@
 import Trash from "../assets/icons/trash.svg";
 import Edit from "../assets/icons/edit.svg";
 import Add from "../assets/icons/add.svg";
+import View from "../assets/icons/eye.svg";
+import NewTab from "../assets/icons/in-new-tab.svg";
 
 export const Icons = {
   trash: Trash,
   edit: Edit,
   add: Add,
+  view: View,
+  newTab: NewTab,
 };
