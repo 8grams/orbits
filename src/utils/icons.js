210bf440cb9cb165299999a6211ee28db3d76df5
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-05-30 02:55:32 +0700

    faet: added icon

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -3,6 +3,11 @@ import Edit from "../assets/icons/edit.svg";
 import Add from "../assets/icons/add.svg";
 import View from "../assets/icons/eye.svg";
 import NewTab from "../assets/icons/in-new-tab.svg";
+import Inventory from "../assets/icons/inventory-management.svg"
+import Playbook from "../assets/icons/playbook.svg"
+import Projects from "../assets/icons/projects.svg"
+import Rocket from "../assets/icons/rocket.svg"
+import Users from "../assets/icons/users.svg" 
 
 export const Icons = {
   trash: Trash,
@@ -10,4 +15,9 @@ export const Icons = {
   add: Add,
   view: View,
   newTab: NewTab,
+  inventory: Inventory,
+  playbook: Playbook,
+  projects: Projects,
+  rocket: Rocket,
+  users: Users
 };
